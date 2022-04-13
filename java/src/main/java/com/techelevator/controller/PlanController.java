eed334c801767a286ff7c1c40fcc85cec792e79d
Justin Long <longjustin1998@gmail.com>
2022-04-13 13:17:08 -0400

    ignore

--- java/src/main/java/com/techelevator/controller/PlanController.java
+++ java/src/main/java/com/techelevator/controller/PlanController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @PreAuthorize("isAuthenticated()")
+@CrossOrigin(origins = "http://localhost:8081")
 public class PlanController {
     private PlanDao planDao;
 

d5f4a2906dda159d12dc194ab81ac512bba996d4
Justin Long <longjustin1998@gmail.com>
2022-04-18 11:42:02 -0400

    Meal styling

--- vue/src/components/PlanDetails.vue
+++ vue/src/components/PlanDetails.vue
@@ -40,9 +40,16 @@ export default {
 
 <style >
 .meal-cards {
-  display: grid;
-  grid-auto-flow: column;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  /* grid-auto-flow: column;
   grid-template-columns: auto auto auto auto auto auto auto;
-  grid-template-rows: auto auto auto;
+  grid-template-rows: auto auto auto; */
+}
+
+.plan-details {
+    display: flex;
+    flex-direction: column;
 }
 </style>

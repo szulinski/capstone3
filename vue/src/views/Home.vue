6ae37dbc97b29b69ece471ccc548db67ef2527cc
Richard Savina <rjsavina@gmail.com>
2022-04-19 14:09:25 -0400

    home

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="home">
-    <h1>Meal Planner v1.0</h1>
+    <img :src="require(`@/images/emptycarrot.png`)" />
+    <h1 class="homeTitle">MealThyme</h1>
     <p>Description text.</p>
     <div class="router-links">
       <router-link class="myrecipes" v-bind:to="{ name: 'my-recipes' }" v-if="$store.state.token != ''">My Recipes</router-link>
       <router-link class="myplans" v-bind:to="{ name: 'my-plans'}" v-if="$store.state.token != ''">My Plans</router-link>
-      <router-link class="groceries" v-bind:to="{name: 'groceries-list'}" v-if="$store.state.token != ''">Groceries List</router-link>
+      <router-link class="groceries" v-bind:to="{name: 'groceries-list'}" v-if="$store.state.token != ''">Grocery Lists</router-link>
     </div>
   </div>
 </template>
@@ -16,30 +17,61 @@ export default {
 };
 </script>
 <style>
+
   .router-links {
     display: flex;
+    text-align: center;
+    
   }
 
   .myrecipes{
+    text-decoration: none;
+    font-size: 34px;
+    -webkit-text-stroke: 1px #5b4b49;
+    -webkit-text-fill-color: #f7bfb4;
     padding: 5%;
     margin: 5%;
     background-color:#23967f;
     border-radius: 100px;
     height: 200px;
     width: 400px;
+    border: 2px solid #5b4b49;
   }
-
   .myplans{
+    text-decoration: none;
+     font-size: 34px;
+    -webkit-text-stroke: 1px #5b4b49;
+    -webkit-text-fill-color: #f7bfb4;
     padding: 5%;
     margin: 5%;
     background-color:#23967f;
     border-radius: 100px;
     height: 200px;
     width: 100px;
+    border: 2px solid #5b4b49;
   }
-
+  .groceries {
+    text-decoration: none;
+     font-size: 34px;
+    -webkit-text-stroke: 1px #5b4b49;
+    -webkit-text-fill-color: #f7bfb4;
+    padding: 5%;
+    margin: 5%;
+    background-color:#23967f;
+    border-radius: 100px;
+    height: 200px;
+    width: 150px;
+    border: 2px solid #5b4b49;
+  }
   .home{
+    
     background-color:#7daf9c;
+    font-family: 'Arial';
+    
   }
-
+  .homeTitle{
+    font-size: 60px;
+    -webkit-text-stroke: 2px #5b4b49;
+    -webkit-text-fill-color: #db93b0;
+  }
 </style>

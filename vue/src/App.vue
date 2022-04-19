acf0a767dab907bceecf23885187e20e4c1c1637
Shae Christensen <shae1199@gmail.com>
2022-04-19 09:45:34 -0400

    nav

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'all-recipes'}" v-if="$store.state.toke != ''">Recipes</router-link>     
+    </div>
     <router-view />
   </div>
 </template>

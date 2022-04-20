d03ea506f20eb16e8347682ec47cd46204d1f60e
Richard Savina <rjsavina@gmail.com>
2022-04-20 13:43:40 -0400

    style

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''"><img class="homeIcon" :src="require(`@/images/home.png`)" />HOME</router-link>
-      <router-link v-bind:to="{ name: 'all-recipes'}" v-if="$store.state.token != ''"><img class="recipesIcon" :src="require(`@/images/recipes.png`)" />RECIPES</router-link>
-      <router-link v-bind:to="{ name: 'my-plans'}" v-if="$store.state.token != ''"><img class="calendarIcon" :src="require(`@/images/calendar.png`)" />PLANS</router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"><img class="logoutIcon" :src="require(`@/images/logout.png`)" />LOGOUT</router-link>
+      <router-link id='home-link' v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''"><img  class="homeIcon" :src="require(`@/images/home.png`)" />HOME</router-link>
+      <router-link id='recipe-link' v-bind:to="{ name: 'all-recipes'}" v-if="$store.state.token != ''"><img class="recipesIcon" :src="require(`@/images/recipes.png`)" />FIND RECIPES</router-link>
+      <router-link id='plans-link' v-bind:to="{ name: 'my-plans'}" v-if="$store.state.token != ''"><img class="calendarIcon" :src="require(`@/images/calendar.png`)" />PLANS</router-link>
+      <router-link id='logout-link' v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"><img class="logoutIcon" :src="require(`@/images/logout.png`)" />LOGOUT</router-link>
 
 
     </div>
@@ -13,52 +13,93 @@
 </template>
 
 <style>
-#link {
-    text-decoration: none;
-}
+
 #nav {
    font-family: 'Arial';
    top: 0;
    height: 50px;
-   width: 100%;
+   width: auto;
    z-index: 1000;
    display: flex;
    position: sticky;
-   justify-content: center;
+   justify-content:  space-evenly;
+   align-items: center;
+
    background-color: #f7bfb4;
+    border-bottom: 2px solid #5b4b49;
+ 
+
 }
-#nav a:hover {
+
+ /* #nav:hover {
   background-color: #5b4b49;
   color: #f7bfb4;
-;
-}
+
+}  */
 .homeIcon{
-    display: block;
+    display: inline-block;
+    vertical-align: bottom;
+    padding-right: 4px;
     margin-left: auto;
     margin-right: auto;
     height: 20px;
     width: 20px;
+    
   }
+ 
+  #home-link {
+    color: #5b4b49;
+  }
+  #home-link:hover {
+    text-shadow: 1px 1px black;
+   
+  }
+  
   .recipesIcon{
-    display: block;
+   display: inline-block;
+    vertical-align: bottom;
+    padding-right: 4px;
+    margin-left: auto;
+    margin-right: auto;
+    height: 20px;
+    width: 20px;
+    
+  }
+  #recipe-link{
+    color: #5b4b49;
+  }
+   #recipe-link:hover{
+     text-shadow: 1px 1px black;
+   }
+  .calendarIcon{
+    display: inline-block;
+    vertical-align: bottom;
+    padding-right: 4px;
     margin-left: auto;
     margin-right: auto;
     height: 20px;
     width: 20px;
   }
-  .calendarIcon{
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    height: 20px;
-    width: 20px;
+  #plans-link{
+    color: #5b4b49;
+  }
+  #plans-link:hover{
+    text-shadow: 1px 1px black;
   }
   .logoutIcon{
-    display: block;
+    display: inline-block;
+    vertical-align: bottom;
+    padding-right: 4px;
     margin-left: auto;
     margin-right: auto;
     height: 20px;
     width: 20px;
+  }
+  #logout-link {
+    color: #5b4b49;
+  }
+  #logout-link:hover{
+    text-shadow: 1px 1px black;
   }
 
 </style>
